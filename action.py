76dda0205b9fff70e109e920973d43614475d8cc
lavender010101 <lavender010101@163.com>
2022-07-03 10:51:07 +0800

    init

--- action.py
+++ action.py
@@ -8,6 +8,7 @@ right_front_wheel = 1
 left_rear_wheel = 1
 right_rear_wheel = 1
 
+test_pin = 1
 
 def forward():
     print("forward")
@@ -20,3 +21,8 @@ def backward():
 
 def forward_left():
     pass
+
+def read_pin(pin):
+    pass
+
+
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import action
+# import action
 
-action.forward()
+import RPi.GPIO as GPIO
+import time
+
+pin = 1
+GPIO.setup(pin, GPIO.IN)
+while True:
+    if GPIO.input(pin) == GPIO.LOW:
+        print("low")
+    time.sleep(1000)
