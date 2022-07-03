ae54f563f466a63c21691ac67666d3204f40984c
lavender010101 <lavender010101@163.com>
2022-07-03 11:34:29 +0800

    init

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import RPi.GPIO as GPIO  #导入RPi.GPIO库
 
 import time  #导入time库
 
-pin = 17  #接声音传感器
+pin = 12  #接声音传感器
 
-GPIO.setmode(GPIO.BCM)  #设置引脚为BCM
+GPIO.setmode(GPIO.BORAD)  #设置引脚为BCM
 
 GPIO.setup(pin, GPIO.IN)  #设置引脚为输入模式
 
