74dbfd0ee8e9a50f91ef8eab4d6da38bcca7bb19
lavender010101 <lavender010101@163.com>
2022-07-03 11:44:59 +0800

    init

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import time  #导入time库
 
 GPIO.setmode(GPIO.BOARD)
 
-pin20 = 20  #接声音传感器
-GPIO.setup(pin20, GPIO.IN)  #设置引脚为输入模式
+pin21 = 21  #接声音传感器
+GPIO.setup(pin21, GPIO.IN)  #设置引脚为输入模式
 # GPIO.setup(pin21, GPIO.IN)  #设置引脚为输入模式
 
 while True:
@@ -17,7 +17,7 @@ while True:
     # else:
     #     print("high")
 
-    print("pin20 -> " + GPIO.input(pin20))
+    print("pin21 -> " + GPIO.input(pin21))
     # print("pin21 -> " + GPIO.input(pin21))
 
     time.sleep(2)  #一秒一次
