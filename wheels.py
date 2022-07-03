ca61321a2dc52642bc181709fa4527eeaf13ec46
lavender010101 <lavender010101@163.com>
2022-07-03 16:41:32 +0800

    wheels

--- wheels.py
+++ wheels.py
@@ -8,6 +8,13 @@ PIN_IN1_R = 15
 PIN_IN2_R = 13
 
 
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PIN_IN1_L, GPIO.OUT)
+    GPIO.setup(PIN_IN2_L, GPIO.OUT)
+    GPIO.setup(PIN_IN1_R, GPIO.OUT)
+    GPIO.setup(PIN_IN2_R, GPIO.OUT)
+
 def forward():
     GPIO.output(PIN_IN1_L, GPIO.HIGH)
     GPIO.output(PIN_IN2_L, GPIO.LOW)
@@ -16,6 +23,7 @@ def forward():
 
 
 if __name__ == "__main__":
+    init()
     try:
         while True:
             forward()
