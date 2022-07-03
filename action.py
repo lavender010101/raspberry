df2e8f981f56eb32da98edd51f2d60803c009931
lavender010101 <lavender010101@163.com>
2022-07-03 10:35:16 +0800

    init

---
+++ action.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+# define the pin of wheels
+left_front_wheel = 1
+right_front_wheel = 1
+left_rear_wheel = 1
+right_rear_wheel = 1
+
+
+def forward():
+    print("forward")
+    # pass
+
+
+def backward():
+    pass
+
+
+def forward_left():
+    pass
