0d782ef0c544ed66165a2851a6cebe71eca4d7d4
lavender010101 <lavender010101@163.com>
2022-07-03 11:15:24 +0800

    init

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-# import action
-
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BOARD)
+import RPi.GPIO as GPIO  #导入库
 
 pin = 1
-GPIO.setup(pin, GPIO.IN)
-while True:
-    if GPIO.input(pin, True):
-        print("true")
-    time.sleep(1000)
+GPIO.setmode(GPIO.BOARD)  # 引入针脚模式BMC或者BOARD模式
+
+GPIO.setup(pin, GPIO.IN)  #设置引脚为输入
+
+GPIO.setup(pin, GPIO.OUT)  #设置引脚为输出
+
+GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)  #设置初始化为高电平
+
+GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)  #设置初始化为低电平
