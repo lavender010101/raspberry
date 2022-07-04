9e21d3618e87f0b6e6b2e7203c31496656752674
lavender010101 <lavender010101@163.com>
2022-07-04 09:01:40 +0800

    beep test

--- beep.py
+++ beep.py
@@ -1,29 +1,79 @@
+#-*- coding: utf-8 -*
 import RPi.GPIO as GPIO
 import time
 
 
-def setup(pin, limit):
-    pass
-    GPIO.setmode(GPIO.BOARD)
-    beep = pin
+class Buzzer_Song(object):
+    # pin_buzzer是IO引脚， delay_beat是一个音持续的时间（节拍时长控制）
+    def __init__(self, pin_buzzer, delay_beat=0.5):
+        # 设置蜂鸣器引脚模式
+        self.pin_buzzer = pin_buzzer
+        GPIO.setup(self.pin_buzzer, GPIO.OUT)
+        # 创建PWM对象初始频率 440hz，占空比50%
+        self.Buzzer = GPIO.PWM(pin_buzzer, 440)
+        self.Buzzer.start(50)
+        # 音符到频率的转换字典，cl低音，cm中音，ch高音
+        self.note2freq = {
+            "cl1": 131,
+            "cl2": 147,
+            'cl3': 165,
+            "cl4": 175,
+            "cl5": 196,
+            "cl6": 211,
+            "cl7": 248,
+            "cm1": 262,
+            "cm2": 294,
+            'cm3': 330,
+            "cm4": 350,
+            "cm5": 393,
+            "cm6": 441,
+            "cm7": 495,
+            "ch1": 525,
+            "ch2": 589,
+            'ch3': 661,
+            "ch4": 700,
+            "ch5": 786,
+            "ch6": 882,
+            "ch7": 990
+        }
+        # 节拍时长初始化
+        self.delay_beat = delay_beat
 
-    GPIO.setup(beep, GPIO.OUT)
+    def play_song(self, notes, beats):
+        for note, beat in zip(notes, beats):
+            # 切换频率，演奏音乐
+            self.Buzzer.ChangeFrequency(self.note2freq[note])
+            # 持续的时间
+            time.sleep(self.delay_beat * beat)
 
-    global pwm
-    pwm = GPIO.PWM(beep, limit)
-    # pwm.ChangeDutyCycle(limit)
-    pwm.start(0)
+    # 对象销毁
+    def destroy(self):
+        self.Buzzer.stop()
+        GPIO.output(self.pin_buzzer, GPIO.LOW)
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
-    pin = 11
-    limit = 100
-    setup(pin, limit)
-
+    # 设置引脚编号模式
+    GPIO.setmode(GPIO.BOARD)
+    # 定义buzzer引脚
+    pin_buzzer = 11
+    # 定义一个对象 m_buzzer_song
+    m_buzzer_song = Buzzer_Song(pin_buzzer, 0.3)
+    notes = [
+        'cm1', 'cm1', 'cm1', 'cl5', 'cm3', 'cm3', 'cm3', 'cm1', 'cm1', 'cm3',
+        'cm5', 'cm5', 'cm4', 'cm3', 'cm2', 'cm2', 'cm3', 'cm4', 'cm4', 'cm3',
+        'cm2', 'cm3', 'cm1', 'cm1', 'cm3', 'cm2', 'cl5', 'cl7', 'cm2', 'cm1'
+    ]
+    beats = [
+        1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 3, 1, 1, 2, 2, 1, 1, 2, 2, 1,
+        1, 2, 2, 1, 1, 3
+    ]
+    # 循环演奏音乐
     try:
         while True:
-            pwm.ChangeDutyCycle(100)
-            time.sleep(1)
+            m_buzzer_song.play_song(notes, beats)
     except KeyboardInterrupt:
-        print("Exiting...")
-    GPIO.cleanup()
+        print('\n Ctrl + C QUIT')
+    finally:
+        m_buzzer_song.destroy()
